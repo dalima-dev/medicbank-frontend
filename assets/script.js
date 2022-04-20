c0f61f39558913f05eae210abebaf7504f46be3b
Daniel Alves <dalima.dev@gmail.com>
2022-04-20 20:41:58 -0300

    feat

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ const baseURL = `https://medicbank-server.herokuapp.com/medic`;
 
 async function findAllMedics() {
   //Here we push our medic list data into HTML.
-  const response = await fetch(`${baseURL}/find-medics`, { mode: 'cors' });
+  const response = await fetch(`${baseURL}/find-medics`);
   const medics = await response.json();
 
   const medicList = document.querySelector('#medicList');
@@ -32,9 +32,7 @@ async function findAllMedics() {
 findAllMedics();
 
 async function findMedicById(idMedic) {
-  const response = await fetch(`${baseURL}/find-medics/${idMedic}`, {
-    mode: 'cors ',
-  });
+  const response = await fetch(`${baseURL}/find-medics/${idMedic}`);
   const medic = await response.json();
 
   let specialties = '';
@@ -144,9 +142,7 @@ async function openModalRegisterUpdate(status, idMedic) {
     </button>
     `;
 
-    const response = await fetch(`${baseURL}/find-medics/${idMedic}`, {
-      mode: 'cors',
-    });
+    const response = await fetch(`${baseURL}/find-medics/${idMedic}`);
     const medic = await response.json();
 
     document.querySelector('#name').value = medic.name;
